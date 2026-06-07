37c2c278320fc0029f05c5c6041afe510c4193c0
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2026-06-07 13:00:26 -0700

    v75.0.0

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -8,6 +8,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Playground\Make\OpenAPI;
 
+use Playground\ServiceProvider;
 use Playground\Test\OrchestraTestCase;
 
 /**
@@ -20,7 +21,7 @@ class TestCase extends OrchestraTestCase
     protected function getPackageProviders($app)
     {
         return [
-            \Playground\ServiceProvider::class,
+            ServiceProvider::class,
             \Playground\Make\ServiceProvider::class,
             \Playground\Make\OpenAPI\ServiceProvider::class,
         ];
